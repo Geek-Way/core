65022c2203c15a01527288a025a5b22d21b77a87
Fabricio Raphael Baliza <fabricioraphael.b@gmail.com>
2021-01-16 22:01:24 -0300

    :pencil: [style] alterando paginas basicas

--- src/main/webapp/app/modules/account/register/register.tsx
+++ src/main/webapp/app/modules/account/register/register.tsx
@@ -102,11 +102,11 @@ export const RegisterPage = (props: IRegisterProps) => {
               <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
             </a>
             <span>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
+              {/* <Translate contentKey="global.messages.info.authenticated.suffix">
                 , you can try the default accounts:
                 <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
                 <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
+              </Translate> */}
             </span>
           </Alert>
         </Col>
--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -17,7 +17,7 @@ export const Home = (props: IHomeProp) => {
     <Row>
       <Col md="9">
         <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
+          <Translate contentKey="home.title">Welcome, Geek Way!</Translate>
         </h2>
         <p className="lead">
           <Translate contentKey="home.subtitle">This is your homepage</Translate>
@@ -37,11 +37,11 @@ export const Home = (props: IHomeProp) => {
               <Link to="/login" className="alert-link">
                 <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
               </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
+              {/* <Translate contentKey="global.messages.info.authenticated.suffix">
                 , you can try the default accounts:
                 <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
                 <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
+              </Translate> */}
             </Alert>
 
             <Alert color="warning">
@@ -58,35 +58,34 @@ export const Home = (props: IHomeProp) => {
 
         <ul>
           <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              <Translate contentKey="home.link.homepage">JHipster homepage</Translate>
+            <a href="https://geekway.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              <Translate contentKey="home.link.homepage">homepage</Translate>
             </a>
           </li>
-          <li>
+          {/* <li>
             <a href="http://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
               <Translate contentKey="home.link.stackoverflow">JHipster on Stack Overflow</Translate>
             </a>
-          </li>
-          <li>
+          </li> */}
+          {/* <li>
             <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
               <Translate contentKey="home.link.bugtracker">JHipster bug tracker</Translate>
             </a>
-          </li>
-          <li>
+          </li> */}
+          {/* <li>
             <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
               <Translate contentKey="home.link.chat">JHipster public chat room</Translate>
             </a>
-          </li>
-          <li>
+          </li> */}
+          {/* <li>
             <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
               <Translate contentKey="home.link.follow">follow @jhipster on Twitter</Translate>
             </a>
-          </li>
+          </li> */}
         </ul>
-
         <p>
           <Translate contentKey="home.like">If you like JHipster, do not forget to give us a star on</Translate>{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Geek-Way/core" target="_blank" rel="noopener noreferrer">
             Github
           </a>
           !
